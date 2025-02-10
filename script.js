7438be0a9837d42e048b28499cc90f8f4a659c9a
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-02-10 13:06:14 -0500

    add user created books to library array and display book on webpage

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+const container = document.querySelector(".container")
 const bookCard = document.querySelector(".books-container");
 const dialog = document.querySelector(".dialog");
 const showButton = document.querySelector(".add-book");
-const closeButton = document.querySelector(".close-form")
+const closeButton = document.querySelector(".close-form");
+const submitButton = document.querySelector(".submit");
+const form = document.querySelector("form")
 
-const myLibrary = [{ title: "lil nao and his journey", author: "JAJU", pages: 100, have_read: true }, { title: "anna and her broccoli", author: "bella", pages: 200, have_read: false }, { title: "the epic downfall of white chocolate", author: "shiver and frye", pages: 333, have_read: true }];
+const myLibrary = [];
 
 function Book(title, author, pages, have_read) {
   this.title = title;
@@ -18,11 +21,12 @@ function addBookToLibrary(title, author, pages, have_read) {
 }
 
 function displayBook() {
-  for (book of myLibrary) {
+  bookCard.innerHTML = "";
+  for (let book of myLibrary) {
     let card = document.createElement("div")
     card.classList.add("book")
     bookCard.appendChild(card)
-    for (key in book) {
+    for (let key in book) {
       let newBook = document.createElement("div")
       newBook.classList.add(`${key}`)
       newBook.textContent = `${key}: ${book[key]}`
@@ -39,4 +43,19 @@ closeButton.addEventListener("click", () => {
   dialog.close();
 })
 
-displayBook();
+submitButton.addEventListener("click", () => {
+  let newBookArray = [];
+  const newBookItem = document.querySelectorAll("input")
+  newBookItem.forEach(item => {
+    if (item.type === "checkbox") {
+      let haveRead = item.checked;
+      newBookArray.push(haveRead)
+    } else {
+      let bookValue = item.value;
+      newBookArray.push(bookValue)
+    }
+  })
+  form.reset();
+  addBookToLibrary(...newBookArray);
+  displayBook();
+})
