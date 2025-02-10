c0828391cc9a2e36009a7f9c5a2668c22350f21c
Nathan Y <193931003+nyamashiro@users.noreply.github.com>
2025-02-10 14:31:41 -0500

    remove the selected book from array and webpage

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const closeButton = document.querySelector(".close-form");
 const submitButton = document.querySelector(".submit");
 const form = document.querySelector("form")
 
-const myLibrary = [];
+const myLibrary = [{title: "toy story 1", author: "anna", pages: 454, have_read: false}, {title: "toy story 2", author: "nao", pages: 232, have_read: true}, {title: "toy story 3", author: "anandsd", pages: 232, have_read: false}];
 
 function Book(title, author, pages, have_read) {
   this.title = title;
@@ -22,18 +22,33 @@ function addBookToLibrary(title, author, pages, have_read) {
 
 function displayBook() {
   bookCard.innerHTML = "";
-  for (let book of myLibrary) {
-    let card = document.createElement("div")
-    card.classList.add("book")
+  for (let [index, book] of myLibrary.entries()) {
+    let card = document.createElement("div");
+    card.classList.add("book");
+    card.dataset.id = index;
     bookCard.appendChild(card)
     for (let key in book) {
-      let newBook = document.createElement("div")
-      newBook.classList.add(`${key}`)
-      newBook.textContent = `${key}: ${book[key]}`
-      card.appendChild(newBook)
+      let newBook = document.createElement("div");
+      newBook.classList.add(`${key}`);
+      newBook.textContent = `${key}: ${book[key]}`;
+      card.appendChild(newBook);
     }
+    let remove = document.createElement("button")
+    remove.classList.add("remove-button")
+    remove.textContent = "Remove"
+    card.appendChild(remove)
   }
 }
+
+bookCard.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-button")) {
+    const selectedBook = e.target.closest(".book")
+
+    const bookKey = selectedBook.dataset.id
+    myLibrary.splice(bookKey, 1)
+    displayBook();
+  }
+})
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
@@ -59,3 +74,5 @@ submitButton.addEventListener("click", () => {
   addBookToLibrary(...newBookArray);
   displayBook();
 })
+
+displayBook()
